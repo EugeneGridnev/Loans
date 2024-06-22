b2293ffb4bcbbd1c46c4766813f2cf08436692af
EugeneGridnev <eugenegrid@yandex.ru>
2024-06-22 23:28:30 +0300

    SpecialOfferFragment added

---
+++ app/src/main/java/com/shift/shiftfinal/ui/SpecialOfferFragment.kt
@@ -0,0 +1,12 @@
+package com.shift.shiftfinal.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.shift.shiftfinal.R
+
+class SpecialOfferFragment : Fragment() {
+
+}
