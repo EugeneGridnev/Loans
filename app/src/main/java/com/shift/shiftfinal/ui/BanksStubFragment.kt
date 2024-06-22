0268eac8b5c494fa7385e7a2374cad7771bf5558
EugeneGridnev <eugenegrid@yandex.ru>
2024-06-22 18:30:46 +0300

    banks stub fragment added

---
+++ app/src/main/java/com/shift/shiftfinal/ui/BanksStubFragment.kt
@@ -0,0 +1,37 @@
+package com.shift.shiftfinal.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.shift.shiftfinal.R
+import com.shift.shiftfinal.databinding.FragmentBanksStubBinding
+import com.shift.shiftfinal.databinding.FragmentLoanAcceptedBinding
+
+class BanksStubFragment : Fragment() {
+    private var _binding: FragmentBanksStubBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentBanksStubBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
