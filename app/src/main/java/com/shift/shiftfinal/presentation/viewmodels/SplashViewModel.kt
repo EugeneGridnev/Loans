a70acf7dbcade350231fa509dae7a001324c000d
EugeneGridnev <eugenegrid@yandex.ru>
2024-06-24 16:33:01 +0300

    view model added added

---
+++ app/src/main/java/com/shift/shiftfinal/presentation/viewmodels/SplashViewModel.kt
@@ -0,0 +1,50 @@
+package com.shift.shiftfinal.presentation.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.shift.shiftfinal.domain.exceptions.ApiException
+import com.shift.shiftfinal.domain.exceptions.AuthException
+import com.shift.shiftfinal.domain.usecase.CheckUserLoginUseCase
+import com.shift.shiftfinal.domain.usecase.GetLoanConditionUseCase
+import com.shift.shiftfinal.domain.usecase.LogoutUserUseCase
+import com.shift.shiftfinal.presentation.navigation.SplashRouter
+import com.shift.shiftfinal.presentation.state.SplashScreenState
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class SplashViewModel @Inject constructor(
+    private val splashRouter: SplashRouter,
+    private val loanConditionUseCase: GetLoanConditionUseCase,
+    private val checkUserLoginUseCase: CheckUserLoginUseCase,
+    private val logoutUserUseCase: LogoutUserUseCase
+) : ViewModel() {
+
+    private val _state = MutableLiveData<SplashScreenState>(SplashScreenState.Loading)
+    val state: LiveData<SplashScreenState> = _state
+
+    fun checkUserLoggedIn() {
+        viewModelScope.launch {
+            _state.value = SplashScreenState.Loading
+            if (!checkUserLoginUseCase()) {
+                delay(1000)
+                splashRouter.openLoginScreen()
+                return@launch
+            }
+            val loanConditions = try {
+                loanConditionUseCase()
+            } catch (e: AuthException) {
+                logoutUserUseCase()
+                splashRouter.openLoginScreen()
+                return@launch
+            }
+            catch (_: ApiException) {
+                _state.value = SplashScreenState.Error("Сервис временно не работает!")
+                return@launch
+            }
+            splashRouter.openHomeScreen(loanConditions)
+        }
+    }
+}
